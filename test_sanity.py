b921e269c6d9c5287017249f7024897309c78f29
yflenner <yoniflenner.net@gmail.com>
2026-03-15 20:22:50 +0200

    added sanity test and github actions workflow

--- test_sanity.py
+++ test_sanity.py
@@ -1,1 +1,37 @@
-print("hello world!")
+#test_open_weather_map.py
+import json
+
+import pytest
+from playwright.sync_api import Playwright
+
+WEATHER_API_URL="https://api.openweathermap.org/data/2.5/weather"
+CITY_NAME="Jerusalem,IL"
+API_KEY="ad48510a9aed1ff96b51557d94bc5964"
+UNITS="metric"
+class Test_Open_Weather_Map:
+    @pytest.fixture(scope="class",autouse=True)
+    def setup(self,playwright:Playwright):
+        global request_context
+        request_context=playwright.request.new_context(base_url=WEATHER_API_URL)
+        yield
+        request_context.dispose()
+
+    def test_get_request(self):
+       api_paramas=dict(appid=API_KEY,q=CITY_NAME,units=UNITS)
+       response=request_context.get(url="",params=api_paramas)
+       result=response.json()
+       print(json.dumps(result,indent=2))
+       print(f"Status Code: {response.status}")
+       print(f"Content Type: {response.headers['content-type']}")
+       print(f"Date: {response.headers['date']}")
+       assert "json" in response.headers["content-type"]
+
+    def test_parsing_json(self):
+        api_paramas = dict(appid=API_KEY, q=CITY_NAME, units=UNITS)
+        response = request_context.get(url="", params=api_paramas)
+        weather_data = response.json()
+        print(f"Country is: {weather_data['sys']['country']}")
+        print(f"Humidity is: {weather_data['main']['humidity']}")
+        print(f"Temperature is: {weather_data['main']['temp']}")
+        assert weather_data["sys"]["country"]=="IL"
+
